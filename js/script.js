640eb494a67fbf27d8b1925780c4463088a95e75
ClaraBayon <cv5bgdmg6c@privaterelay.appleid.com>
2026-01-12 13:12:35 +0100

    commit.initial

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const concierto = new Date ("2026-06-11T21:00:00"); // cambia a túa data/hora
+const days = document.getElementById("dias");
+const hours = document.getElementById("horas");
+const minutes = document.getElementById("minutos");
+const seconds = document.getElementById("segundos");
+
+
+function actualizarContaAtras() {
+ const agora = new Date();
+ const comezaConcerto = concierto - agora; // milisegundos que faltan
+
+ if (comezaConcerto <= 0) {
+ console.log("¡El concierto ha comenzado!");
+ clearInterval(id);
+ return;
+ }
+ // Dividir entre 1000, 60, 60, 24 converte milisegundos → segundos → minutos → horas → días
+ const segundos = Math.floor(comezaConcerto / 1000) % 60;
+ const minutos = Math.floor(comezaConcerto / 1000 / 60) % 60;
+ const horas = Math.floor(comezaConcerto / 1000 / 60 / 60) % 24;
+ const dias = Math.floor(comezaConcerto / 1000 / 60 / 60 / 24);
+
+ days.textContent = dias;
+ hours.textContent = horas;
+ minutes.textContent = minutos;
+ seconds.textContent = segundos;
+
+  console.log(
+ `Faltan ${dias}d ${horas}h ${minutos}m ${segundos}s para o concerto`
+ );
+}
+
+ 
+
+actualizarContaAtras(); // unha vez ao inicio
+const id = setInterval(actualizarContaAtras, 1000); // logo cada segundo
+
+
+// Modal
+const modal = document.getElementById("modal");
+
